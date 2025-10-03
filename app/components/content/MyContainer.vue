581c3c984c6f91a4bb5992153b974dc60af07e1b
Vladislav <tomasciuc.vladislav@gmail.com>
2025-10-03 14:13:00 +0300

    upd pages

---
+++ app/components/content/MyContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="mx-auto page-width-wide:mx-0 w-full decoration-primary/6 print:break-inside-avoid max-w-3xl page-width-wide:max-w-screen-2xl page-api-block:ml-0 flex flex-row gap-3 justify-center"
+  >
+    <div
+      class="relative overflow-hidden after:block after:absolute after:-inset-0 after:pointer-events-none w-fit"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ app/components/content/StaticImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center justify-center">
+    <NuxtImg :src="image" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  image: string;
+}>();
+</script>
+
+<style scoped></style>
